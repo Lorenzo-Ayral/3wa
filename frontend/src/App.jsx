4be057a53155b6f5deb9755fa1e800ddcdad0ff7
Lorenzo <62961070+Lorenzo-Ayral@users.noreply.github.com>
2023-08-30 18:19:58 +0200

    Add react router, mailhog and pages (#4)
    Co-authored-by: Lorenzo Ayral <lorenzo.ayral@digitalseeder.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css'
+import {HomePage} from './pages/HomePage';
+import Header from "./components/Header";
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route exact path="/" element={<HomePage/>}/>
+                    <Route path="/about" element={<AboutPage/>} />
+                    <Route path="/contact" element={<ContactPage/>} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+function AboutPage() {
+    return (
+        <div>
+            <h2>À propos de nous</h2>
+            <p>
+                Bienvenue sur notre site web "À propos de nous". Ici, vous pouvez en apprendre davantage sur notre entreprise
+                et notre équipe.
+            </p>
+        </div>
+    );
+}
+
+export default AboutPage;
