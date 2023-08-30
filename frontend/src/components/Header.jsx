473a95e7311404ec8546204c9c943d38d0c54600
Lorenzo Ayral <lorenzo.ayral@digitalseeder.com>
2023-08-30 18:19:21 +0200

    Add react router, mailhog and pages

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">À propos</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ frontend/src/pages/ContactPage.jsx
@@ -0,0 +1,17 @@
+function ContactPage() {
+    return (
+        <div>
+            <h2>Contactez-nous</h2>
+            <p>
+                Vous pouvez nous contacter en utilisant les coordonnées ci-dessous :
+            </p>
+            <ul>
+                <li>Email : contact@exemple.com</li>
+                <li>Téléphone : +1234567890</li>
+                <li>Adresse : 123 Rue de la Paix, Paris, France</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ContactPage;
